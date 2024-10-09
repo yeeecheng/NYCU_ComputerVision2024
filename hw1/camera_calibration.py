173ed1b11da795852b794720939f6ac34013becb
yeeecheng <yichengliao1003@gmail.com>
2024-10-10 04:44:47 +0800

    hw1 final code

--- hw1/camera_calibration.py
+++ hw1/camera_calibration.py
@@ -1,124 +1,251 @@
-import numpy as np
+import argparse
 import cv2
-import glob
 import matplotlib.pyplot as plt
+import numpy as np
+import os 
 import camera_calibration_show_extrinsics as show
 from PIL import Image
 from mpl_toolkits.mplot3d import Axes3D
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-# (8,6) is for the given testing images.
-# If you use the another data (e.g. pictures you take by your smartphone), 
-# you need to set the corresponding numbers.
-corner_x = 7
-corner_y = 7
-objp = np.zeros((corner_x*corner_y,3), np.float32)
-objp[:,:2] = np.mgrid[0:corner_x, 0:corner_y].T.reshape(-1,2)
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d points in real world space
-imgpoints = [] # 2d points in image plane.
+class CameraCalibration:
 
-# Make a list of calibration images
-images = glob.glob('data/*.jpg')
+    def __init__(self, data_dir, n_corner_x, n_corner_y):
+        
+        # Number of corner point where black and white meet in the checkerboard image.
+        self.n_corner_x = n_corner_x
+        self.n_corner_y = n_corner_y
+        # Read image path
+        self.img_path_list = [os.path.join(data_dir, file) for file in os.listdir(data_dir)]
+        print("Total number of images are ", len(self.img_path_list))
 
-# Step through the list and search for chessboard corners
-print('Start finding chessboard corners...')
-for idx, fname in enumerate(images):
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    plt.imshow(gray)
+    def get_world_points_and_image_points(self):
+        # the criteria of refined corners
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        # Creating list to store 2D image points in image coordinate system.
+        self.list_images_points_2d = []
+        # Define the 3D world points in world coordinate system, there is a trick that only create 2d array because of z = 0.
+        self.world_points_3d = np.zeros((self.n_corner_x * self.n_corner_y, 2), np.float32)
+        # Prepare world points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0). 
+        self.world_points_3d[:, :2] = np.mgrid[0 : self.n_corner_x, 0 : self.n_corner_y].T.reshape(-1, 2)
+        
+        for fname in self.img_path_list:
+            
+            img = cv2.imread(fname)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # Get the all of the image points in the checkerboard image.
+            print('find the chessboard corners of',fname)
+            # corners: all coordination of image points.
+            ret, corners = cv2.findChessboardCorners(gray, (self.n_corner_x, self.n_corner_y), None)
+            if ret == True:
+                # Refined pixel coordinate for 2d points.
+                corners_refined = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+                self.list_images_points_2d.append(corners_refined.reshape(-1, 2))
+                # Draw and display the corners
+                img = cv2.drawChessboardCorners(img, (self.n_corner_x, self.n_corner_y), corners_refined, ret)
 
-    #Find the chessboard corners
-    print('find the chessboard corners of',fname)
-    ret, corners = cv2.findChessboardCorners(gray, (corner_x,corner_y), None)
+        self.world_points_3d = np.array(self.world_points_3d)
+        self.list_images_points_2d = np.array(self.list_images_points_2d)
+        print("3D world points shape: ", self.world_points_3d.shape)
+        print("2D image points shape: ", self.list_images_points_2d[0].shape)
 
-    # If found, add object points, image points
-    if ret == True:
-        objpoints.append(objp)
-        imgpoints.append(corners)
+    def __get_homography(self, img_points):
+        """ Gets Homography matrix each image"""
 
-        # Draw and display the corners
-        cv2.drawChessboardCorners(img, (corner_x,corner_y), corners, ret)
-        plt.imshow(img)
+        # Create matrix L
+        L = []
+        for idx in range(img_points.shape[0]):
+            u, v = img_points[idx]
+            X, Y = self.world_points_3d[idx]
+            L.append(np.array([-X, -Y, -1, 0, 0, 0, u * X, u * Y, u]))
+            L.append(np.array([0, 0, 0, -X, -Y, -1, v * X, v * Y, v]))
 
+        # Apply SVD
+        L = np.array(L)
+        _, _, V_T = np.linalg.svd(L, True)
+        V = V_T.T
+        # last column of V corresponds to smallest eigen value
+        H = V[:, -1]
+        # scale ambiguity
+        H = H / H[8]
+        # H shape is 3 by 3
+        H = np.reshape(H, (3, 3))
 
-#######################################################################################################
-#                                Homework 1 Camera Calibration                                        #
-#               You need to implement camera calibration(02-camera p.76-80) here.                     #
-#   DO NOT use the function directly, you need to write your own calibration function from scratch.   #
-#                                          H I N T                                                    #
-#                        1.Use the points in each images to find Hi                                   #
-#                        2.Use Hi to find out the intrinsic matrix K                                  #
-#                        3.Find out the extrensics matrix of each images.                             #
-#######################################################################################################
-print('Camera calibration...')
-img_size = img[0].shape
-# You need to comment these functions and write your calibration function from scratch.
-# Notice that rvecs is rotation vector, not the rotation matrix, and tvecs is translation vector.
-# In practice, you'll derive extrinsics matrixes directly. The shape must be [pts_num,3,4], and use them to plot.
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
-Vr = np.array(rvecs)
-Tr = np.array(tvecs)
-print(mtx)
-print(Vr.shape)
-print(Tr.shape)
-extrinsics = np.concatenate((Vr, Tr), axis=1).reshape(-1,6)
-print(extrinsics.shape)
-"""
-Write your code here
+        return H
 
+    def get_all_homography(self):
+        """ Gets Homography of all images present in calibration images dataset """
+        
+        list_H = []
 
+        for img_points in self.list_images_points_2d:
+            H = self.__get_homography(img_points)
+            list_H.append(H)
 
+        return np.array(list_H)
 
-"""
-# show the camera extrinsics
-print('Show the camera extrinsics')
-# plot setting
-# You can modify it for better visualization
-fig = plt.figure(figsize=(10, 10))
-# ax = fig.gca(projection='3d')
-ax = fig.add_axes(Axes3D(fig))   
-# camera setting
-camera_matrix = mtx
-cam_width = 0.064/0.1
-cam_height = 0.032/0.1
-scale_focal = 1600
-# chess board setting
-board_width = 8
-board_height = 6
-square_size = 1
-# display
-# True -> fix board, moving cameras
-# False -> fix camera, moving boards
-min_values, max_values = show.draw_camera_boards(ax, camera_matrix, cam_width, cam_height,
-                                                scale_focal, extrinsics, board_width,
-                                                board_height, square_size, True)
+    def __get_Vij(self, hi, hj):
 
-X_min = min_values[0]
-X_max = max_values[0]
-Y_min = min_values[1]
-Y_max = max_values[1]
-Z_min = min_values[2]
-Z_max = max_values[2]
-max_range = np.array([X_max-X_min, Y_max-Y_min, Z_max-Z_min]).max() / 2.0
+        Vij = np.array([
+            hi[0] * hj[0],
+            hi[0] * hj[1] + hi[1] * hj[0],
+            hi[1] * hj[1],
+            hi[2] * hj[0] + hi[0] * hj[2],
+            hi[2] * hj[1] + hi[1] * hj[2],
+            hi[2] * hj[2]
+        ])
 
-mid_x = (X_max+X_min) * 0.5
-mid_y = (Y_max+Y_min) * 0.5
-mid_z = (Z_max+Z_min) * 0.5
-ax.set_xlim(mid_x - max_range, mid_x + max_range)
-ax.set_ylim(mid_y - max_range, 0)
-ax.set_zlim(mid_z - max_range, mid_z + max_range)
+        return Vij.T
 
-ax.set_xlabel('x')
-ax.set_ylabel('z')
-ax.set_zlabel('-y')
-ax.set_title('Extrinsic Parameters Visualization')
-plt.show()
+    def __get_V(self, list_H):
+        
+        V = []
+        # solve to V of all image, reference formula (8)
+        for H in list_H:
+            # the first column of H
+            h1 = H[:, 0]
+            # the second column of H
+            h2 = H[:, 1]
 
-#animation for rotating plot
-"""
-for angle in range(0, 360):
-    ax.view_init(30, angle)
-    plt.draw()
-    plt.pause(.001)
-"""
+            v12 = self.__get_Vij(h1, h2)
+            v11 = self.__get_Vij(h1, h1)
+            v22 = self.__get_Vij(h2, h2)
+            V.append(v12.T)
+            V.append((v11 - v22).T)
+
+        # shape (2 * num_images, 6)
+        return np.array(V) 
+
+    def __get_B(self, list_H):
+        """ B is symmetric matrix """
+        V = self.__get_V(list_H)
+        _, _, C_T = np.linalg.svd(V)
+        b = C_T.T[:, -1] 
+
+        # get B with b = [B11, B12, B22, B13, B23, B33]
+        B = np.array([
+            [b[0], b[1], b[3]],
+            [b[1], b[2], b[4]],
+            [b[3], b[4], b[5]]
+        ])
+
+        return B
+
+    def get_intrinsic_matrix(self, list_H):
+        """" Get intrinsic matrix """
+        # Get B
+        B = self.__get_B(list_H)
+        print("----Estimated B Matrix is ----\n", B)
+        # Get intrinsic matrix, which compute all parameters of intrinsic matrix from matrix B
+        v0 = (B[0, 1] * B[0, 2] - B[0, 0] * B[1, 2]) / (B[0, 0] * B[1, 1] - B[0, 1] ** 2)
+        lambd = (B[2, 2] - (B[0, 2] ** 2 + v0 * (B[0, 1] * B[0, 2] - B[0, 0] * B[1, 2])) / B[0, 0])
+        alpha = np.sqrt(lambd / B[0, 0])
+        beta = np.sqrt((lambd * B[0, 0]) / (B[0, 0] * B[1, 1] - B[0, 1] ** 2))
+        gamma = -1 * B[0, 1] * (alpha ** 2) * (beta) / lambd
+        u0 = (gamma * v0 / beta) - (B[0, 2] * (alpha ** 2) / lambd)
+
+        # Create Intrinsic matrix A
+        A = np.array([
+            [alpha, gamma, u0],
+            [0, beta, v0],
+            [0, 0, 1]
+        ])
+
+        return A
+
+    def get_extrinsic_matrix(self, A, list_H):
+        """ Get extrinsic matrix which is the rotation and translation of the each image """
+        rvecs = []
+        tvecs = []
+
+        for H in list_H:
+            # Get the i-th column of H
+            h1 = H[:, 0]
+            h2 = H[:, 1]
+            h3 = H[:, 2]
+            # Get extrinsic matrix, which compute all parameters of extrinsic matrix from matrix H and A
+            lambd = 1 / np.linalg.norm(np.matmul(np.linalg.pinv(A), h1), 2)
+            r1 = np.matmul(lambd * np.linalg.pinv(A), h1)
+            r2 = np.matmul(lambd * np.linalg.pinv(A), h2)
+            r3 = np.cross(r1, r2)
+            t = np.matmul(lambd * np.linalg.pinv(A), h3)
+            # cv2.Rodrigus can transform matrix to vector
+            rvec, _ = cv2.Rodrigues(np.vstack((r1, r2, r3)).T)
+            rvecs.append(rvec)
+            tvecs.append(np.vstack((t)))
+        
+        return rvecs, tvecs
+
+def visualize(A, rvecs, tvecs):
+    """ Visualize picture """
+
+    Vr = np.array(rvecs)
+    Tr = np.array(tvecs)
+    extrinsics = np.concatenate((Vr, Tr), axis=1).reshape(-1,6)
+
+    # show the camera extrinsics
+    print('Show the camera extrinsics')
+    # plot setting
+    # You can modify it for better visualization
+    fig = plt.figure(figsize=(10, 10))
+    # ax = fig.gca(projection='3d')
+    ax = fig.add_axes(Axes3D(fig))   
+    # camera setting
+    camera_matrix = A
+    cam_width = 0.064/0.1
+    cam_height = 0.032/0.1
+    scale_focal = 1600
+    # chess board setting
+    board_width = 8
+    board_height = 6
+    square_size = 1
+    # display
+    # True -> fix board, moving cameras
+    # False -> fix camera, moving boards
+    min_values, max_values = show.draw_camera_boards(ax, camera_matrix, cam_width, cam_height,
+                                                    scale_focal, extrinsics, board_width,
+                                                    board_height, square_size, True)
+    X_min = min_values[0]
+    X_max = max_values[0]
+    Y_min = min_values[1]
+    Y_max = max_values[1]
+    Z_min = min_values[2]
+    Z_max = max_values[2]
+    max_range = np.array([X_max-X_min, Y_max-Y_min, Z_max-Z_min]).max() / 2.0
+
+    mid_x = (X_max+X_min) * 0.5
+    mid_y = (Y_max+Y_min) * 0.5
+    mid_z = (Z_max+Z_min) * 0.5
+    ax.set_xlim(mid_x - max_range, mid_x + max_range)
+    ax.set_ylim(mid_y - max_range, 0)
+    ax.set_zlim(mid_z - max_range, mid_z + max_range)
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('z')
+    ax.set_zlabel('-y')
+    ax.set_title('Extrinsic Parameters Visualization')
+    plt.show()
+
+def run_camera_calibration(args):
+
+    camera_calibration = CameraCalibration(args.data, args.corner_x, args.corner_y)
+    camera_calibration.get_world_points_and_image_points() 
+    list_H = camera_calibration.get_all_homography()
+    A = camera_calibration.get_intrinsic_matrix(list_H)
+    print("----Estimated intrinsic matrix is ----\n", A)
+    rvecs, tvecs = camera_calibration.get_extrinsic_matrix(A, list_H)
+    print("----Estimated rotate vector of extrinsic matrix is ----\n", rvecs)
+    print("----Estimated translation vector of extrinsic matrix is ----\n", tvecs)
+    visualize(A, rvecs, tvecs)
+
+def parse_args():
+    parse = argparse.ArgumentParser()
+    parse.add_argument('-d', '--data', default= "./data/7by10", type= str, help= 'path of data dir')
+    parse.add_argument('-x', '--corner_x', default= 10, type= int, help= 'Number of corner point where black and white meet in the pattern.')
+    parse.add_argument('-y', '--corner_y', default= 7, type= int, help= 'Number of corner point where black and white meet in the pattern.')
+    args = parse.parse_args()
+    return args
+    
+
+if __name__ == "__main__":
+    args = parse_args()
+    run_camera_calibration(args)
